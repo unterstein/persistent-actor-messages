a754d131b63a948da69413ebe5ab0b30b999ec8f
Johannes Unterstein <unterstein@me.com>
2016-03-13 21:41:42 +0100

    removed actor

--- src/main/scala/com/example/PingActor.scala
+++ src/main/scala/com/example/PingActor.scala
@@ -6,18 +6,11 @@ class PingActor extends Actor with ActorLogging {
   import PingActor._
   
   var counter = 0
-  val pongActor = context.actorOf(PongActor.props, "pongActor")
 
   def receive = {
   	case Initialize => 
 	    log.info("In PingActor - starting ping-pong")
-  	  pongActor ! PingMessage("ping")	
-  	case PongActor.PongMessage(text) =>
-  	  log.info("In PingActor - received message: {}", text)
-  	  counter += 1
-  	  if (counter == 3) context.system.shutdown()
-  	  else sender() ! PingMessage("ping")
-  }	
+d  }
 }
 
 object PingActor {
--- src/test/scala/com/example/PingPongActorSpec.scala
+++ src/test/scala/com/example/PingPongActorSpec.scala
@@ -1,12 +1,8 @@
 package com.example
 
 import akka.actor.ActorSystem
-import akka.actor.Actor
-import akka.actor.Props
-import akka.testkit.{ TestActors, TestKit, ImplicitSender }
-import org.scalatest.WordSpecLike
-import org.scalatest.Matchers
-import org.scalatest.BeforeAndAfterAll
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
  
 class PingPongActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
@@ -20,16 +16,7 @@ class PingPongActorSpec(_system: ActorSystem) extends TestKit(_system) with Impl
   "A Ping actor" must {
     "send back a ping on a pong" in {
       val pingActor = system.actorOf(PingActor.props)
-      pingActor ! PongActor.PongMessage("pong")
       expectMsg(PingActor.PingMessage("ping"))
-    }
-  }
-
-  "A Pong actor" must {
-    "send back a pong on a ping" in {
-      val pongActor = system.actorOf(PongActor.props)
-      pongActor ! PingActor.PingMessage("ping")
-      expectMsg(PongActor.PongMessage("pong"))
     }
   }
 
