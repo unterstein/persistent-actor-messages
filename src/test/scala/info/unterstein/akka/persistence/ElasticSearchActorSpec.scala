0af930460b4d86714dd55c8cec858ed880755d31
Johannes Unterstein <unterstein@me.com>
2016-03-15 11:25:51 +0100

    renaming

--- src/test/scala/info/unterstein/akka/persistence/ElasticSearchActorSpec.scala
+++ src/test/scala/info/unterstein/akka/persistence/ElasticSearchActorSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 /**
  * @author Johannes Unterstein (unterstein@me.com)
  */
-class PingPongActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
+class ElasticSearchActorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
  
   def this() = this(ActorSystem("MySpec"))
